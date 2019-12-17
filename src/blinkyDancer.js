42e03ba86570cf02d3f70cf3a56f6d8ae25c3159
almostgone47 <almostgone47@hotmail.com>
2019-12-16 18:26:34 -0800

    Test working
    Not sure why

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,9 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
+  // this.step();
   // inherted props = this.$node = $('<span class="dancer"></span>');
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -12,11 +16,17 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  this.oldStep();
+
+  //We need to call step again
+
+  // this.oldStep();
+
+  this.$node.toggle();
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
 };
 
 // let newDancer = makeBlinkyDancer(10, 20, 1);
