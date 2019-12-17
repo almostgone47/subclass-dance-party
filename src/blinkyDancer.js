7ea7a86a088173d4c8bf43974fcec9e51eacc77e
almostgone47 <almostgone47@hotmail.com>
2019-12-17 12:31:37 -0800

    Implement blinky

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,7 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);
+var makeBlinkyDancer = function(bottom, left, timeBetweenSteps) {
+  makeDancer.call(this, bottom, left, timeBetweenSteps);
+  this.$node = $('<img class="blinkyDancer" src="https://cdn3.iconfinder.com/data/icons/halloween-2142/128/casper_friendly_ghost_halloween_haunt_pacman_spirit-512.png">');
+  this.setPosition(bottom, left);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
