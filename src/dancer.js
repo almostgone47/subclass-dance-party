572276c2dcaeddf6bed5036af3260860db17a3ec
Alex Kim <alexkim612@gmail.com>
2019-12-16 22:50:14 -0800

    Take out top and left properties from object

--- src/dancer.js
+++ src/dancer.js
@@ -1,10 +1,8 @@
 var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
-  this.top = top;
-  this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   this.step();
-  this.setPosition(this.top, this.left);
+  this.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
