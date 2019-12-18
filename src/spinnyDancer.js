70f9c59c2c9ad3303b6dc166ba09b81f7264d413
almostgone47 <almostgone47@hotmail.com>
2019-12-17 16:28:38 -0800

    Change picture from  miley to cow

--- src/spinnyDancer.js
+++ src/spinnyDancer.js
@@ -1,6 +1,6 @@
 var makeSpinnyDancer = function(bottom, left, timeBetweenSteps) {
   makeDancer.call(this, bottom, left, timeBetweenSteps);
-  this.$node = $('<img class="spinnyDancer" src="https://i1.pngguru.com/preview/802/475/698/hannah-montana-forever-miley-cyrus-png-clipart.jpg">');
+  this.$node = $('<img class="spinnyDancer" src="https://library.kissclipart.com/20181210/ase/kissclipart-cow-transparent-background-clipart-holstein-friesi-aabf16afe16c0010.jpg">');
   this.setPosition(bottom, left);
 };
 
